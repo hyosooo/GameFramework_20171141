da5454fba2016e87edbd9f00b308b88aab46076b
효수 김 <hs_03@naver.com>
2018-09-12 19:55:50 +0900

    2018.09.12 수정

--- PP02.BreakUpHelloSDL/main.cpp
+++ PP02.BreakUpHelloSDL/main.cpp
@@ -38,28 +38,17 @@ void render()
 
 
 
-int main(int argc, char* args[])
+int main(int argc, char* argv[])
 {
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-
-		g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
-
-		if (g_pWindow != 0) {
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-		else {
-			return 1;
-		}
-
-		SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-		SDL_RenderClear(g_pRenderer);
-		SDL_RenderPresent(g_pRenderer);
-		SDL_Delay(5000);
-		SDL_Quit();
-		return 0;
+	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
+		g_bRunning = true;
 	}
+	else {
+		return 1;
+	}
+	while (g_bRunning) {
+		render();
+	}
+	SDL_Quit();
+	return 0;
+}
