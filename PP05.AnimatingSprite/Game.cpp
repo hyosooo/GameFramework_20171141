0883252218e2d960876835c328b96e1c1098c381
user <user@532-14>
2018-09-20 16:58:50 +0900

    2018.09.20 PP05.AnimatingSprite

---
+++ PP05.AnimatingSprite/Game.cpp
@@ -0,0 +1,80 @@
+#include"Game.h"
+#include <iostream>
+using namespace std;
+
+bool Gamee::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,
+			pTempSurface);
+		SDL_FreeSurface(pTempSurface);
+
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
+
+
+	}
+	else {
+		return false; // sdl could not initialize
+	}
+	return true;
+
+}
+
+void Gamee::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Gamee::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture,
+		&m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Gamee::clean()
+{
+	std::cout << "cleaning game" << endl;
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Gamee::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+
+
+
+
