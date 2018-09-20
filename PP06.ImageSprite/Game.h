ba4116898bcaa1f4eca8b9a48f773a9077b24914
user <user@532-14>
2018-09-20 17:28:26 +0900

    2018.09.20 PP06.ImageSprite

---
+++ PP06.ImageSprite/Game.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "SDL.h"
+
+class Gamee
+{
+	SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	SDL_Rect m_sourceRectangle; // 원본 사각형 
+	SDL_Rect m_destinationRectangle; // 대상 사각형 
+public:
+	Gamee() {}
+	~Gamee() {}
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
