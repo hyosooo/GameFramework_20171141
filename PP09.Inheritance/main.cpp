f325d8b4a5fbd2ca096369978ff982cf02b46890
user <user@532-14>
2018-10-18 17:42:06 +0900

    PP09. 수정

--- PP09.Inheritance/main.cpp
+++ PP09.Inheritance/main.cpp
@@ -5,7 +5,7 @@ Game* g_game = 0;
 int main(int argc, char* garv[])
 {
 	g_game = new Game();
-	g_game->init("PP08.SingletonTM", 100, 100, 640, 480, false);
+	g_game->init("PP09.Inheritance", 100, 100, 640, 480, false);
 
 	while (g_game->running())
 	{
